cebf8725a3b11d3b084fe42755127c1b9c48ee92
Ian <ianfarina9@gmail.com>
2025-11-13 18:41:57 -0300

    feat: Integrates MySQL and bcrypt for user authentication

--- config/index.js
+++ config/index.js
@@ -2,5 +2,13 @@ import 'dotenv/config'
 
 export const config = {
     jwtSecret: process.env.JWT_SECRET || 'secretkey',
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    db: {
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD ,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT || 3306
+    }
+
 }
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,27 +1,43 @@
 import { config } from "../config"
 import jwt from 'jsonwebtoken'
+import bcrypt from "bcryptjs"
+import pool from '../config/db.js'
 
-const testUsers = [
-    { id: '123', email: 'user@test.com', password: 'password123' }
-]
 
-function findUserByCredentials(email, password) {
-    return testUsers.find(user => user.email === email && user.password === password) // With DB, this will use bcrypt and mysql
-}
-
-export const loginHandler = (req, res) => { // Handler for login route
+export const loginHandler = async (req, res) => { // Handler for login route
     const { email, password } = req.body // validating inputs
     if (!email || !password) {
         return res.status(400).json({ error: 'email and password are required' })
     }
 
-    const user = findUserByCredentials(email, password)
+    try {
+        const sql = "SELECT * FROM users WHERE email = ?" // Query to find user by email
+        const [rows] = await pool.query(sql, [email]) // Pool.query give [rows, fields]. I'll only use rows
 
-    if (!user) {
-        return res.status(401).json({ error: 'Invalid email or password' })
+        if (rows.length === 0) {
+            console.log('Failed login: No user found');
+            return res.status(401).json({ error: 'Invalid email or password' })
+        }
+
+        const user = rows[0]
+        const isMatch = await bcrypt.compare(password, user.password_hash)
+
+        if (!isMatch) {
+            console.log('Failed login: Password does not match');
+            return res.status(401).json({ error: 'Invalid email or password' })
+        }
+
+        const payload = { email: user.email, id: user.id } // Information saved in the token if login is successful
+        const accessToken = jwt.sign(
+            payload,
+            config.jwtSecret,
+            { expiresIn: '1h' }
+        )
+
+        res.json({ accessToken: accessToken })
+
+    } catch (error) {
+        console.log('Login error', error);
+        return res.status(500).json({ error: 'Internal server error' })
     }
-
-    const payload = { email: user.email, id: user.id } // Information saved in the token if login is successful
-    const accessToken = jwt.sign(payload, config.jwtSecret, { expiresIn: '1h' })
-    res.json({ accessToken })
 }
