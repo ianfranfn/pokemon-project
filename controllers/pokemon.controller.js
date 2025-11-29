0e36177d6e3db30a94af6b75ce1a0965b2c607ee
Ian <ianfarina9@gmail.com>
2025-11-28 21:06:24 -0300

    refactor: Implemented handler of specific exceptions on getPokemonHandler

--- controllers/pokemon.controller.js
+++ controllers/pokemon.controller.js
@@ -41,3 +41,33 @@ export const deletePokemonHandler = async (req, res) => {
         
     }
 }
+
+export const getPokemonHandler = async (req, res) => {
+    console.log(`got a request to /api/pokemon by user: ${req.user.email}`)
+    try { // Try to get the user's Pokemons
+        const userId = req.user.id
+        const pokemons = await getUserPokemons(userId) // Get Pokemons from the service
+
+        if (pokemons.length === 0) {
+            try {
+                const newPokemon = await addDittoToUser(userId)
+                return res.status(201).json([newPokemon]) 
+            } catch (creationError) { // Captures the specific error of creation (external API or DB)
+                console.error('Error adding new Pokemon:', creationError.message);
+                return res.status(503).json({
+                    error: 'External service unavailable',
+                    details: creationError.message
+                })
+            }
+        }
+
+        return res.status(200).json(pokemons)
+
+    } catch (error) { // If there is an error contacting the external API
+        console.error('Error fetching Pokemons:', error.message)
+        return res.status(500).json({
+            error: 'Internal database error during read',
+            details: error.message
+        })   
+    }
+}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -13,27 +13,6 @@ router.get('/home', verifyToken, (req, res) => {
     res.send(`Welcome to the page, ${req.user.email}!`)
 })
 
-router.get('/api/pokemon', verifyToken, async (req, res) => {
-    console.log(`got a request to /api/pokemon by user: ${req.user.email}`)
-    try { // Try to get the user's Pokemons
-        const pokemons = await getUserPokemons(req.user.id) // Get Pokemons from the service
-
-        if (pokemons.length === 0) {
-            const newPokemon = await addDittoToUser(req.user.id)
-            return res.json([newPokemon])
-        }
-
-        return res.json(pokemons)
-
-    } catch (error) { // If there is an error contacting the external API
-        console.error('Error contacting PokeAPI:', error.message)
-        res.status(500).json({
-            error: 'Cannot contact PokeAPI',
-            details: error.message
-        })   
-    }
-})
-
 router.put('/api/pokemon/:id', verifyToken, updatePokemonHandler)
 
 router.delete('/api/pokemon/:id', verifyToken, deletePokemonHandler)
