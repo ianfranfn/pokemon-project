f0197c44387e18ecffa39d5da742c8258127b611
Ian <ianfarina9@gmail.com>
2026-02-12 22:40:10 -0300

    feat: add swagger documentation and enable CORS

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -13,6 +13,38 @@ router.get('/home', verifyToken, (req, res) => {
     res.send(`Welcome to the page, ${req.user.email}!`)
 })
 
+/**
+ * @swagger
+ * /api/pokemon:
+ *  get:
+ *    summary: Get the list of all Pokemon
+ *    tags: [Pokemons]
+ *    security:
+ *    - bearerAuth: []
+ *    responses:
+ *      200:
+ *        description: List of Pokemon obtained correctly
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                  id:
+ *                    type: integer
+ *                    example: 1
+ *                  name:
+ *                    type: string
+ *                    example: "Pikachu"
+ *                  image:
+ *                    type: string
+ *                    example: "http://imagen.com/pikachu.png"
+ *      401:
+ *        description: Not authorized (Token Missing)
+ */
+
+
 router.get('/api/pokemon', verifyToken, getPokemonHandler)
 router.put('/api/pokemon/:id', verifyToken, updatePokemonHandler)
 router.delete('/api/pokemon/:id', verifyToken, deletePokemonHandler)
